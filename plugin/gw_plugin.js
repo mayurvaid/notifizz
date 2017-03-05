06c91f9b554349f77d810dc64bdbffb0df816fc9
ababeen <ababeen135@gmail.com>
2017-03-06 00:33:23 +0530

    Update gw_plugin.js

--- plugin/gw_plugin.js
+++ plugin/gw_plugin.js
@@ -1,4 +1,4 @@
-
+var domain_url = "//localhost/project/plugin/";
 if (document.getElementById('gw_placeholder')) {
 	document.body.removeChild(document.getElementById('gw_placeholder'));
 }
@@ -13,10 +13,12 @@ for (var i = 0; i < gw_img.length; i++) {
 	}
 }
 var bodyhtml = document.body.innerHTML;
+bodyhtml+="<input type='hidden' id='windowlocation' value='"+window.location.href+"'/>";
 
 function gw_iframeLoaded() {
 	var win = document.getElementById('gw_iframe').contentWindow;
 	win.postMessage(bodyhtml, "*");
+
 
 }
 
@@ -27,7 +29,7 @@ window.onclick = function(event) {
 	}
 }
 // Put the object into storage
-var html = "<style> .gw_modal { display: none; position: fixed; z-index: 1; padding-top: 100px; left: 0; top: 0; width: 100%; height: 100%; overflow: auto; background-color: rgb(0,0,0); background-color: rgba(0,0,0,0.4); z-index:100000; } .gw_modal-content { position: relative; background-color: #fefefe; margin: auto; padding: 0; border: 1px solid #888; width: 80%; height: 80%; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19); -webkit-animation-name: animatetop; -webkit-animation-duration: 0.4s; animation-name: animatetop; animation-duration: 0.4s } @-webkit-keyframes animatetop { from {top:-300px; opacity:0} to {top:0; opacity:1} } @keyframes animatetop { from {top:-300px; opacity:0} to {top:0; opacity:1} } .gw_close { color: white; float: right; font-size: 28px; font-weight: bold; } .gw_close:hover, .gw_close:focus { color: #000; text-decoration: none; cursor: pointer; } .gw_modal-body {padding: 2px 16px;} </style> <div id='gw_modal' class='gw_modal'> <div class='gw_modal-content'> <iframe id='gw_iframe' src='//localhost/notifiz/plugin/iframe.php' width='100%' height='100%' onload='gw_iframeLoaded(this)'></iframe> </div> </div>";
+var html = "<style> .gw_modal { display: none; position: fixed; z-index: 1; padding-top: 100px; left: 0; top: 0; width: 100%; height: 100%; overflow: auto; background-color: rgb(0,0,0); background-color: rgba(0,0,0,0.4); z-index:100000; } .gw_modal-content { position: relative; background-color: #fefefe; margin: auto; padding: 0; border: 1px solid #888; width: 80%; height: 80%; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19); -webkit-animation-name: animatetop; -webkit-animation-duration: 0.4s; animation-name: animatetop; animation-duration: 0.4s } @-webkit-keyframes animatetop { from {top:-300px; opacity:0} to {top:0; opacity:1} } @keyframes animatetop { from {top:-300px; opacity:0} to {top:0; opacity:1} } .gw_close { color: white; float: right; font-size: 28px; font-weight: bold; } .gw_close:hover, .gw_close:focus { color: #000; text-decoration: none; cursor: pointer; } .gw_modal-body {padding: 2px 16px;} </style> <div id='gw_modal' class='gw_modal'> <div class='gw_modal-content'> <iframe id='gw_iframe' src='"+domain_url+"/iframe.php' width='100%' height='100%' onload='gw_iframeLoaded(this)'></iframe> </div> </div>";
 var elem = document.createElement("div");
 elem.id = 'gw_placeholder';
 elem.innerHTML = html;
